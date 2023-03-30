fb2afed22701b2e9f22c3587e2d4528c02d835f7
Pritchard Oluwafeyisanumi <104890172+Pritchard94@users.noreply.github.com>
2023-03-30 18:30:02 +0100

    fix

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -15,42 +15,42 @@ const Albums = () => {
         <div className='flex items-center border-b-4 border-[#F20A0A] justify-between'>
             <h6>Music</h6>
             <div className='flex items-center pb-2'>
-                <img src={bar1} alt='bar'/>
-                <img className='ml-3' src={bar2} alt ='bar2'/>
+                <img className='w-[20px]' src={bar1} alt='bar'/>
+                <img className='ml-3 w-[20px]' src={bar2} alt ='bar2'/>
             </div>
         </div>
         <div className='grid grid-cols-2 md:grid-cols-3 place-items-center gap-5 md:gap-10 mt-4'>
             <div className='relative'>
                 <img src={first} alt='first'/>               
-                <p className='absolute top-[85%] text-white text-[20px] bg-[#222222a2] w-full text-center py-3'>Abby Jasmine - "First Day Out"</p>
+                <p className='absolute top-[85%]  text-white text-[8px] md:text-white lg:text-[20px] bg-[#222222a2] w-full text-center lg:py-3 py-[3px]'>Abby Jasmine - "First Day Out"</p>
             </div>
             <div className='relative'>
                 <img src={rema} alt='rema'/>               
-                <p className='absolute top-[85%] text-white text-[20px] bg-[#222222a2] w-full text-center py-3'>Bad Gyal - "44"</p>
+                <p className='absolute top-[85%] text-white text-[8px] lg:text-[20px] bg-[#222222a2] w-full text-center lg:py-3 py-[3px]'>Bad Gyal - "44"</p>
             </div>
             <div className='relative'>
                 <img src={blanc} alt='blanco'/>               
-                <p className='absolute top-[85%] text-white text-[20px] bg-[#222222a2] w-full text-center py-3'>Benny Blanco - "Found You"</p>
+                <p className='absolute top-[85%] text-white text-[8px] lg:text-[20px] bg-[#222222a2] w-full text-center lg:py-3 py-[3px]'>Benny Blanco - "Found You"</p>
             </div>
             <div className='relative'>
                 <img src={way} alt='way out'/>               
-                <p className='absolute top-[85%] text-white text-[20px] bg-[#222222a2] w-full text-center py-3'>Contradash - "Way Out"</p>
+                <p className='absolute top-[85%] text-white text-[8px] lg:text-[20px] bg-[#222222a2] w-full text-center lg:py-3 py-[3px]'>Contradash - "Way Out"</p>
             </div>
             <div className='relative'>
                 <img src={dif} alt='difference'/>               
-                <p className='absolute top-[85%] text-white text-[20px] bg-[#222222a2] w-full text-center py-3'>Co Cash - "Difference"</p>
+                <p className='absolute top-[85%] text-white lg:text-[20px] bg-[#222222a2] w-full text-center  lg:py-3 py-[3px] text-[8px]'>Co Cash - "Difference"</p>
             </div>
             <div className='relative'>
                 <img src={miss} alt='missing'/>               
-                <p className='absolute top-[85%] text-white text-[20px] bg-[#222222a2] w-full text-center py-3'>Kitty Cash - "Missing You"</p>
+                <p className='absolute top-[85%] text-white lg:text-[20px] bg-[#222222a2] w-full text-center lg:py-3 py-[3px] text-[8px]'>Kitty Cash - "Missing You"</p>
             </div>
             <div className='relative'>
                 <img src={tooley} alt='brokeasf'/>               
-                <p className='absolute top-[85%] text-white text-[20px] bg-[#222222a2] w-full text-center py-3'>BrokeASF - "Tooley"</p>
+                <p className='absolute top-[85%] text-white lg:text-[20px] bg-[#222222a2] w-full text-center lg:py-3 py-[3px] text-[8px]'>BrokeASF - "Tooley"</p>
             </div>
             <div className='relative'>
                 <img src={numb} alt='numb'/>               
-                <p className='absolute top-[85%] text-white text-[20px] bg-[#222222a2] w-full text-center py-3'>Brianna Castro - "Numb"</p>
+                <p className='absolute top-[85%] text-white lg:text-[20px] bg-[#222222a2] w-full text-center lg:py-3 py-[3px] text-[8px]'>Brianna Castro - "Numb"</p>
             </div>
 
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import logo from '../assets/logo.svg'
 import { Link } from 'react-router-dom'
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 
 const Header = () => {
   return (
